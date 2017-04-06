c3c4871e3f75b6bae180b60bb82a282e620ed47b
김보운 <haioer@naver.com>
2017-04-06 17:12:40 +0900

    azure storage images

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -3,8 +3,21 @@ Routes and views for the flask application.
 """
 
 from FlaskWebProject1 import app
+from azure.storage.blob import BlobService
+from flask import redirect
+
+def azureStorageList():
+    urlPath = None
+    block_blob_service = BlobService(account_name='azurestoragepractice',
+                                     account_key='nAL6kanC+Y71Jfe1PRP8Z5V/8nrd/YVbGTd2gpF8tKZ45gndqI94WqUdZajdoFzO0geKWKA8OqAeA18mDt2Zcw==')
+
+    generator = block_blob_service.list_blobs('azure-practice')
+    for blob in generator:
+        urlPath = block_blob_service.make_blob_url('azure-practice', blob.name)
+
+    return urlPath
 
 @app.route('/')
 def home():
     """Renders the home page."""
-    return "<h1>Hello, Flask!</h1>"
+    return redirect(azureStorageList())
